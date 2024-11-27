3ab797188f6ee82df4a9b1430eafe12a082caa94
VneTraffiqua <safe72again@gmail.com>
2024-11-27 01:54:46 -0800

    Added fake bus generator

--- fake_bus.py
+++ fake_bus.py
@@ -1,42 +1,39 @@
 import trio
-from sys import stderr
 import json
 from trio_websocket import open_websocket_url, ConnectionClosed
-import itertools
 import os
 
 
-def load_routes(directory_path='routes'):
+SLEEP_SEC=2
+
+
+def load_routes(directory_path):
     for filename in os.listdir(directory_path):
         if filename.endswith(".json"):
             filepath = os.path.join(directory_path, filename)
             with open(filepath, 'r', encoding='utf8') as file:
                 yield json.load(file)
 
-def read_json_file(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
-
-
 async def broadcast_bus_route(route):
     while True:
         try:
             async with open_websocket_url('ws://127.0.0.1:8080') as ws:
-                    for item in route['coordinates']:
-                        message = json.dumps(
-                                {"busId": route['name'], "lat": item[0], "lng": item[1], "route": route['name']},
-                        )
-                        await ws.send_message(message)
-                    await trio.sleep(1)
+                for item in route['coordinates']:
+                    message = json.dumps(
+                        {"busId": route['name'], "lat": item[0], "lng": item[1], "route": route['name']},
+                        ensure_ascii=False
+                    )
+                    await ws.send_message(message)
+                    await trio.sleep(SLEEP_SEC)
+                    print(message)
         except ConnectionClosed:
             break
 
 async def main():
-    # route = read_json_file('./156.json')
-    # await broadcast_bus_route(route)
     async with trio.open_nursery() as nursery:
-        for route in load_routes('./routes'):
+        for route in load_routes('routes-1'):
             nursery.start_soon(broadcast_bus_route, route)
 
 
-trio.run(main)
+if __name__ == '__main__':
+    trio.run(main)
