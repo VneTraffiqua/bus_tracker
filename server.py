d6247ff95edb7a97ad2b69dedfc3d68d668d8492
VneTraffiqua <safe72again@gmail.com>
2024-11-27 01:55:47 -0800

    Sent buses data to browser

--- server.py
+++ server.py
@@ -1,38 +1,40 @@
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
+from functools import partial
 import json
-import aiofiles
-import itertools
 
 
-def read_json_file(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
-
-
-async def send_message(request):
+async def get_coordinates(request, buses_on_map):
     ws = await request.accept()
-
     while True:
         try:
             message = await ws.get_message()
-            # message = json.dumps({
-            #     "msgType": "Buses",
-            #     "buses": [
-            #         {"busId": "в420ор", "lat": item[0], "lng": item[1], "route": bus_data['name']},
-            #         {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
-            #         {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
-            #     ]
-            # })
-            # await ws.send_message(message)
-            print(message)
-            await trio.sleep(1)
+            message = json.loads(message)
+            buses_on_map[message['busId']] = message
+            # await trio.sleep(1)
         except ConnectionClosed:
             break
 
 
+async def talk_to_browser(request, buses_on_map):
+    response = {
+        "msgType": "Buses",
+        "buses": []
+    }
+    ws = await request.accept()
+    for key, value in buses_on_map.items():
+        response['buses'].append(value)
+    print(response)
+    await ws.send_message(json.dumps(response))
+
+
 async def main():
-    await serve_websocket(send_message, '127.0.0.1', 8080, ssl_context=None)
+    buses = {}
+    get_buses_coordinates = partial(get_coordinates, buses_on_map=buses)
+    send_buses_coordinates_to_browser = partial(talk_to_browser, buses_on_map=buses)
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(serve_websocket, get_buses_coordinates, '127.0.0.1', 8080, None)
+        nursery.start_soon(serve_websocket, send_buses_coordinates_to_browser, '127.0.0.1', 8000, None)
 
 
 if __name__ == "__main__":
