cda45b38f6499d079ed0c5a439d4023b6e43548b
VneTraffiqua <safe72again@gmail.com>
2024-11-29 02:48:27 -0800

    Launched many bus routes

--- fake_bus.py
+++ fake_bus.py
@@ -2,10 +2,14 @@ import trio
 import json
 from trio_websocket import open_websocket_url, ConnectionClosed
 import os
+import itertools
+from random import randint
+
+SLEEP_SEC=1
 
 
-SLEEP_SEC=2
-
+def generate_bus_id(route_id, bus_index):
+    return f"{route_id}-{bus_index}"
 
 def load_routes(directory_path):
     for filename in os.listdir(directory_path):
@@ -18,9 +22,19 @@ async def broadcast_bus_route(route):
     while True:
         try:
             async with open_websocket_url('ws://127.0.0.1:8080') as ws:
-                for item in route['coordinates']:
+                random_start = randint(0, len(route['coordinates']))
+                bus_route_with_random_start = itertools.cycle(
+                    itertools.islice(route['coordinates'], random_start, None)
+                )
+                bus_id = generate_bus_id(route['name'], randint(0, 1000))
+                for item in bus_route_with_random_start:
                     message = json.dumps(
-                        {"busId": route['name'], "lat": item[0], "lng": item[1], "route": route['name']},
+                        {
+                            "busId": bus_id,
+                            "lat": item[0],
+                            "lng": item[1],
+                            "route": route['name']
+                        },
                         ensure_ascii=False
                     )
                     await ws.send_message(message)
@@ -32,7 +46,8 @@ async def broadcast_bus_route(route):
 async def main():
     async with trio.open_nursery() as nursery:
         for route in load_routes('routes-1'):
-            nursery.start_soon(broadcast_bus_route, route)
+            for _ in range(3):
+                nursery.start_soon(broadcast_bus_route, route)
 
 
 if __name__ == '__main__':
